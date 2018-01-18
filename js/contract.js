8d33047e7c001613aff4f91e327220a35a4650a9
anirudhshenoy <anirudhshenoy92@gmail.com>
2018-01-18 21:51:43 +0530

    Initial Commit

---
+++ js/contract.js
@@ -0,0 +1,5 @@
+var account = '0x87b3f6def4d451c41be733b8924da66dea0caed4'; // Dev
+var contractAddress = '0x58b671784f4fa6b02e3dcac9f9dd215b66b5669b';
+
+var web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
+console.log("hello test")
